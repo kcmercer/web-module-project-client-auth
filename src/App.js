8708c88dea73ef16d06a0425038c902b729f1ac0
Kenneth Mercer <kmercer5439@gmail.com>
2022-01-25 17:27:27 -0600

    completed login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import styled from 'styled-components';
+
+import Login from './components/login';
+
+const StyledDiv = styled.div`
+  display: flex;
+  border-bottom: 6px solid black;
+  height: auto;
+  width: 100%;
+`
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  display: flex;
+  justify-content: flex-end;
+  height: 6em;
+  width: 100%;
+`
+
+const StyledLi = styled.li`
+  font-size: 1.5rem;
+  margin: 0.5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  width: 10rem;
+  color: white;
+  background-color: black;
+`
+
+const StyledH1 = styled.h1`
+  margin-top: 0%;
+  margin-bottom: 0%;
+  font-size: 3rem;
+`
 
 function App() {
+  const isLoggedIn = true;
+
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <StyledDiv>
+          <StyledH1>FRIENDS DATABASE</StyledH1>
+          <StyledUl>
+            <StyledLi>
+              <Link style={{ textDecoration: 'none', color: 'white' }} to="/login">LOGIN.</Link>
+            </StyledLi>
+
+            <StyledLi>
+              <Link style={{ textDecoration: 'none', color: 'white' }} to="/logout">LOGOUT.</Link>
+            </StyledLi>
+
+            <StyledLi>
+              {isLoggedIn && <Link style={{ textDecoration: 'none', color: 'white' }} to="/friendlist">FRIENDLIST.</Link>}
+            </StyledLi>
+
+            <StyledLi>
+              {isLoggedIn && <Link style={{ textDecoration: 'none', color: 'white' }} to="/addfriend">ADDFRIEND.</Link>}
+            </StyledLi>
+          </StyledUl>
+        </StyledDiv>
+
+        <br></br>
+
+        <div>
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/login.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-flow: column wrap;
+    align-content: center;
+`
+
+const StyledH1 = styled.h1`
+    font-size: 5rem;
+    font-family: "Times New Roman", Times, serif;
+`
+
+const StyledInput = styled.input`
+    margin-bottom: 5%;
+    height: 2em;
+    width: 16em;
+    background-color: black;
+    border-radius: 6px;
+    color: white;
+    font-size: 2rem;
+`
+
+const StyledLabel = styled.label`
+    margin-right: 65%;
+    font-size: 2.5rem;
+    font-family: "Times New Roman", Times, serif;
+`
+
+const StyledButton = styled.button`
+    margin-top: 4%;
+    height: 2em;
+    width: 14em;
+    background-color: black;
+    color: white;
+    font-size: 2rem;
+    border-radius: 6px;
+    font-family: "Times New Roman", Times, serif;
+`
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+  
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        console.log(this.state.credentials);
+
+        axios.post('http://localhost:9000/api/login', this.state.credentials)
+            .then(resp => {
+                console.log(resp)
+
+                localStorage.setItem("token", resp.data.token)
+                localStorage.setItem("role", resp.data.role)
+                localStorage.setItem("username", resp.data.username);
+
+                this.props.history.push('/FriendsList')
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <StyledDiv>
+                <StyledH1>LOGIN</StyledH1>
+                <form onSubmit={this.login}>
+                    <StyledLabel> USERNAME </StyledLabel>
+                <br></br>
+                    <StyledInput
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                        /> 
+                <br></br>
+                    <StyledLabel> PASSWORD </StyledLabel>
+                <br></br>
+                    <StyledInput
+                        type='password'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                        />
+                  <br></br>      
+                    <StyledButton>SUBMIT</StyledButton>
+                </form>
+            </StyledDiv>
+        )
+    }
+}
+
+export default Login;
